2204b821f87f9ed8d736042fd8f4469d75a6fd09
nasreenmir06 <nasreenmir06@gmail.com>
2024-10-15 17:45:24 -0400

    Set up dropdown feature

--- flightSearch.py
+++ flightSearch.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import csv
 
 app = Flask(__name__)
@@ -8,18 +8,23 @@ def read_csv():
     departureCodes = []
     arrivalCodes = []
     csv_file_path = 'db/flights.csv'
-
+    
+    # read csv file
     with open(csv_file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
+        # skip headings row
+        next(reader)  
         for row in reader:
             if row[2] not in departureCodes:
                 departureCodes.append(row[2])
             if row[3] not in arrivalCodes:
                 arrivalCodes.append(row[3])
-        print(departureCodes)
-        print(arrivalCodes)
-
-    return "CSV file content printed in the terminal!"
+    print(departureCodes)
+    print(arrivalCodes)
+    
+    #pass the lists to html file
+    return render_template('flightSearch.html', departureCodes=departureCodes, arrivalCodes=arrivalCodes)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
