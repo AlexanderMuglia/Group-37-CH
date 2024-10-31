8ce9e2ae4f877486ca290594d9c78655f716ebaa
nasreenmir06 <nasreenmir06@gmail.com>
2024-10-31 12:01:27 -0400

    Added explanatory comments

--- app/flightSearch.py
+++ app/flightSearch.py
@@ -3,6 +3,7 @@ import sqlite3
 
 bp = Blueprint('flightSearch', __name__)
 
+# connect to db
 con = sqlite3.connect("db/main.db", check_same_thread=False)
 cur = con.cursor()
 
@@ -11,6 +12,7 @@ def read_table():
     departureCodes = []
     arrivalCodes = []
 
+    # get departure and arrival codes from flight db
     cur.execute("SELECT DISTINCT departure_code FROM flight")
     departureCodes = [row[0] for row in cur.fetchall()]
 
@@ -19,25 +21,31 @@ def read_table():
 
     return render_template('flightSearch.html', departureCodes=departureCodes, arrivalCodes=arrivalCodes)
 
+# route to handle form submission
 @bp.route('/search', methods=['POST'])
 def search():
     departure = request.form.get('departure')
     arrival = request.form.get('arrival')
 
+    # initialize variables to hold info
     flightId = None
     departureDate = None
     departureTime = None
     price = None
     flightAvailable = False
 
+    # query database to try and find matching flight with given input
     cur.execute("""
         SELECT fid, departure_datetime, price
         FROM flight
         WHERE departure_code = ? AND arrival_code = ?
     """, (departure, arrival))
 
+    # since there's only one matching flight, just get the first matching result
+    # this can be easily changed, i just put this for simplicity
     flight = cur.fetchone()
 
+    # display results accordingly depending on whether there is or isn't a matching flight
     if flight:
         flightAvailable = True
         flightId = flight[0]
